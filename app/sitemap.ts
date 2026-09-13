fbb2fa09f40a22fba4dc3a4d3f810ce90395aa9a
Timileyin20 <98179650+Timileyin20@users.noreply.github.com>
2026-09-13 09:25:00 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl =
+    'https://heavenly-website-rbng-7b6h3xysl-timidoskiakinwole20-9258.vercel.app'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/properties`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/marketplace`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/sell`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/policy`,
+      lastModified: new Date(),
+    },
+  ]
+}
