e6516d1f6194c78a031afb8d482fb797bb2be4cf
Maxi_Brain <98179650+Timileyin20@users.noreply.github.com>
2026-09-16 06:11:09 +0100

    Protect private routes from search crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,18 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: [
+        '/admin/',
+        '/auth/',
+        '/checkout/',
+        '/choose-role/',
+        '/dashboard/',
+        '/forgot-password/',
+        '/orders/',
+        '/signin/',
+        '/signup/',
+        '/update-password/',
+      ],
     },
     sitemap: 'https://heavenly-website-orpin.vercel.app/sitemap.xml',
   }
