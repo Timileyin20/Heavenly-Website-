15ceb4d0c2ed64429aa21bda4f2c7c6a1b2168e8
Maxi_Brain <98179650+Timileyin20@users.noreply.github.com>
2026-09-16 05:30:48 +0100

    Add robots.txt for Google crawling

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://heavenly-website-orpin.vercel.app/sitemap.xml',
+  }
+}
